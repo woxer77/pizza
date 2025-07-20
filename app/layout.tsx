ede035d6261436912fe7913a1fc3e5ee8d675437
Denis Ohrimenko <woxer77@gmail.com>
2025-07-20 14:21:57 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Space_Grotesk } from 'next/font/google';
+import './globals.css';
+
+const spaceGrotesk = Space_Grotesk({
+  variable: '--font-space-grotesk',
+  subsets: ['latin']
+});
+
+export const metadata: Metadata = {
+  title: 'Next Pizza',
+  description: 'Pizza store created with NextJS'
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${spaceGrotesk.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintImport from 'eslint-plugin-import';
+import prettierConfig from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+const eslintConfig = [
+  {
+    ignores: ['dist', 'node_modules', '.next']
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      import: eslintImport
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'no-var': 'error',
+      semi: 'error',
+      'no-multi-spaces': 'error',
+      'space-in-parens': 'error',
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'prefer-const': 'error',
+      'no-use-before-define': 'error',
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'react/jsx-one-expression-per-line': 'off',
+      'linebreak-style': ['off', 'windows'],
+      'comma-dangle': [
+        'error',
+        {
+          imports: 'never'
+        }
+      ],
+      'react/jsx-props-no-spreading': 'warn',
+      'import/no-named-as-default': 'warn',
+      'import/no-named-as-default-member': 'warn',
+      'import/prefer-default-export': 'off',
+      camelcase: 'warn',
+      'react/no-array-index-key': 'warn',
+      eqeqeq: 'warn',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'no-param-reassign': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error'
+    }
+  },
+  eslintConfigPrettier
+];
+
+export default eslintConfig;
