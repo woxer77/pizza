e82c437929e0691f23b2f848f0bfcb277d15e9e3
Denis Ohrimenko <woxer77@gmail.com>
2025-08-02 18:53:22 +0300

    feat: make logo as link to '/'

--- src/components/elements/header.tsx
+++ src/components/elements/header.tsx
@@ -4,6 +4,7 @@ import Search from '@/ui/search';
 import Image from 'next/image';
 import { Button } from '@/ui/button';
 import { User, ShoppingCart, MoveRight } from 'lucide-react';
+import Link from 'next/link';
 
 import type { ClassProps } from '@/types/common';
 import { cn } from '@/lib/utils';
@@ -15,19 +16,13 @@ const Header: React.FC<ClassProps> = ({ className }) => {
   return (
     <header className={cn('w-full border-b-1 border-neutral-300', className)}>
       <div className="container mx-auto flex items-center py-10">
-        <div className="flex shrink-0 gap-x-4">
-          <Image
-            src="/icon.svg"
-            alt="logo"
-            width={42}
-            height={42}
-            priority
-          ></Image>
+        <Link href="/" className="flex shrink-0 gap-x-4">
+          <Image src="/icon.svg" alt="logo" width={42} height={42} priority />
           <div className="flex flex-col">
             <h1 className="text-2xl font-black">NEXT PIZZA</h1>
             <h3 className="-mt-1.5 text-neutral-500">fast. cheap. tasty.</h3>
           </div>
-        </div>
+        </Link>
         <Search placeholder="I would like..." />
         <div className="flex gap-3">
           <Button variant="outline">
