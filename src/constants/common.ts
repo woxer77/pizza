e84984aace81b4941ae1fa25f68bb78faa73bc43
Denis Ohrimenko <woxer77@gmail.com>
2025-08-12 14:34:02 +0300

    feat: added ingredient images and updated ingredient list in common.ts

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -5,88 +5,98 @@ export const topFilters = [
 
 export const ingredients = [
   {
-    name: 'Cheese crust',
-    price: 1.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png'
+    name: 'Bacon',
+    price: 1.29,
+    image: '/ingredients/bacon.png'
   },
   {
-    name: 'Creamy mozzarella',
+    name: 'Bavarian Sausages',
+    price: 1.49,
+    image: '/ingredients/bavarian-sausages.png'
+  },
+  {
+    name: 'Beef',
+    price: 1.59,
+    image: '/ingredients/beef.png'
+  },
+  {
+    name: 'Blue Cheese',
+    price: 1.39,
+    image: '/ingredients/blue-cheese.png'
+  },
+  {
+    name: 'Brynza Cheese',
+    price: 1.19,
+    image: '/ingredients/brynza-cheese.png'
+  },
+  {
+    name: 'Champignons',
+    price: 0.89,
+    image: '/ingredients/champignons.png'
+  },
+  {
+    name: 'Cheddar and Parmesan',
+    price: 1.49,
+    image: '/ingredients/cheddar-and-parmesan.png'
+  },
+  {
+    name: 'Cheese Rim',
+    price: 1.99,
+    image: '/ingredients/cheese-rim.png'
+  },
+  {
+    name: 'Fresh Tomatoes',
     price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png'
-  },
-  {
-    name: 'Cheddar and parmesan cheese',
-    price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796'
-  },
-  {
-    name: 'Spicy jalapeño pepper',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png'
-  },
-  {
-    name: 'Tender chicken',
-    price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA5B328D35A'
-  },
-  {
-    name: 'Mushrooms',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA67259A324'
+    image: '/ingredients/fresh-tomatoes.png'
   },
   {
     name: 'Ham',
-    price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA61B9A8D61'
+    price: 1.29,
+    image: '/ingredients/ham.png'
   },
   {
-    name: 'Spicy pepperoni',
-    price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA6258199C3'
+    name: 'Italian Herbs',
+    price: 0.49,
+    image: '/ingredients/italian-herbs.png'
   },
   {
-    name: 'Spicy chorizo',
-    price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA62D5D6027'
-  },
-  {
-    name: 'Pickled cucumbers',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9EA89958D782B'
-  },
-  {
-    name: 'Fresh tomatoes',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA7AC1A1D67'
-  },
-  {
-    name: 'Red onion',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA60AE6464C'
-  },
-  {
-    name: 'Juicy pineapples',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0'
-  },
-  {
-    name: 'Italian herbs',
-    price: 0.39,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png'
-  },
-  {
-    name: 'Sweet pepper',
-    price: 0.59,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B'
-  },
-  {
-    name: 'Feta cheese cubes',
-    price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA6B0FFC349'
+    name: 'Jalapeno',
+    price: 0.69,
+    image: '/ingredients/jalapeno.png'
   },
   {
     name: 'Meatballs',
+    price: 1.49,
+    image: '/ingredients/meatballs.png'
+  },
+  {
+    name: 'Mozzarella',
+    price: 1.19,
+    image: '/ingredients/mozarella.png'
+  },
+  {
+    name: 'Pickled Cucumbers',
+    price: 0.69,
+    image: '/ingredients/pickled-cucumbers.png'
+  },
+  {
+    name: 'Red Onion',
+    price: 0.59,
+    image: '/ingredients/red-onion.png'
+  },
+  {
+    name: 'Shrimps',
+    price: 2.29,
+    image: '/ingredients/shrimps.png'
+  },
+  {
+    name: 'Spicy Chorizo',
+    price: 1.49,
+    image: '/ingredients/spicy-chorizo.png'
+  },
+  {
+    name: 'Sweet Pepper',
     price: 0.79,
-    image: 'https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png'
+    image: '/ingredients/sweet-pepper.png'
   }
 ];
